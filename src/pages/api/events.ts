c22335911649665db19cf52fcbffaaad5f297602
kfarino <farinooh@gmail.com>
2025-03-28 23:23:37 -0700

    Fix TypeScript errors

--- src/pages/api/events.ts
+++ src/pages/api/events.ts
@@ -1,35 +1,31 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-// Initialize global clients array if it doesn't exist
+declare global {
+  var clients: NextApiResponse[];
+}
+
 if (!global.clients) {
   global.clients = [];
 }
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'GET') {
-    return res.status(405).json({ message: 'Method not allowed' });
+  if (req.method === 'GET') {
+    // Set headers for SSE
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Connection', 'keep-alive');
+
+    // Add this client to our list
+    global.clients.push(res);
+
+    // Remove client when they disconnect
+    req.on('close', () => {
+      const index = global.clients.indexOf(res);
+      if (index !== -1) {
+        global.clients.splice(index, 1);
+      }
+    });
+  } else {
+    res.status(405).json({ message: 'Method not allowed' });
   }
-
-  // Set headers for SSE
-  res.setHeader('Content-Type', 'text/event-stream');
-  res.setHeader('Cache-Control', 'no-cache');
-  res.setHeader('Connection', 'keep-alive');
-
-  // Add this client to the clients list
-  global.clients.push(res);
-
-  // Send initial connection established message
-  res.write('data: {"type":"connected"}\n\n');
-
-  // Keep the connection alive
-  const keepAlive = setInterval(() => {
-    res.write(':\n\n');
-  }, 30000);
-
-  // Clean up on close
-  req.on('close', () => {
-    clearInterval(keepAlive);
-    global.clients = global.clients.filter(client => client !== res);
-    res.end();
-  });
 } 
